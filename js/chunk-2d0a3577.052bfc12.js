76d6228eb0482d22fb7c6bd2df2b7f231733506f
Alan <jxnx888@hotmail.com>
2021-01-18 18:10:53 +0800

    new version 2021/01/18

---
+++ js/chunk-2d0a3577.052bfc12.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0a3577"],{"0287":function(e,t,a){"use strict";a.r(t);var n=function(){var e=this,t=e.$createElement,a=e._self._c||t;return a("div",{staticClass:"aboutme_wrapper"},[e._v(" about me ")])},u=[],s={name:"aboutme"},c=s,o=a("2877"),r=Object(o["a"])(c,n,u,!1,null,"6f9d15f9",null);t["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d0a3577.052bfc12.js.map
